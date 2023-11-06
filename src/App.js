ceb777be2cd17c2645671a7555c712e3817a41fc
Moise Alexandru <moise.alexandru.ichb@gmail.com>
2023-11-06 04:26:34 +0200

    finalizare proiect

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
@@ -34,6 +35,25 @@ export default function App() {
   );
 }
 
-function TextExpander() {
-  return <div>TODO</div>;
+function TextExpander({collapsedNumWords = 10, expandButtonText = "Show more", collapseButtonText = "Show less", buttonColor = '#1f09cd', className, expanded = false, children}) {
+
+    const [isExpanded, setIsExpanded] = useState(expanded);
+
+    const displayText = isExpanded ? children : children.split(' ').slice(0, collapsedNumWords).join(' ') + "...";
+
+    const buttonStyle = {
+        background: 'none',
+        border: 'none',
+        font: 'intherit',
+        cursor: 'pointer',
+        marginLeft: '6px',
+        color: buttonColor
+    }
+
+    return (
+        <div className = {className}>
+            <span> {displayText} </span>
+            <button onClick = {() => setIsExpanded((exp) => !exp)} style = {buttonStyle}> {isExpanded ? collapseButtonText : expandButtonText} </button>
+        </div>
+    );
 }
